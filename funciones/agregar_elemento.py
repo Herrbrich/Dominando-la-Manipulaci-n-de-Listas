53a2da9b21a03613c68fa4349d7f4ea1ae73da40
Lucas Bricchi <lucasbricchi12@gmail.com>
2025-05-15 22:56:27 -0300

    cambios: la realdiad es que no cambie nada del codigo pero me aparece la M y me da ansiedad

--- funciones/agregar_elemento.py
+++ funciones/agregar_elemento.py
@@ -3,4 +3,4 @@ def agregar_elemento(lista :list, elemento: int | float | str)-> list:
     La funcion agrega un elemento al final de la lista y la retorna
     """
     lista += [elemento]
-    return lista
+    return lista 
